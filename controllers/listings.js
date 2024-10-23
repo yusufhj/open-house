bdfe5404a164d7a450c78682ab8ee761afed826f
Yusuf AlHamad <ylive71@gmail.com>
2024-10-23 14:46:46 +0300

    show listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -5,11 +5,14 @@ const Listing = require('../models/listing.js');
 
 router.get('/', async (req, res) => {
     try {
-        const listings = await Listing.find({});
+        const listings = await Listing.find({}).populate('owner');
         console.log('Listing: ', listings);
-        res.render('listings/index.ejs');
+        res.render('listings/index.ejs', {
+            listings,
+        });
     } catch (error) {
         console.log(error);
+        res.redirect('/');
     }
 });
 
@@ -26,6 +29,20 @@ router.post('/', async (req, res) => {
         res.redirect('/listings');
     } catch (error) {
         console.log(error);
+        res.redirect('/listings/new');
+    }
+});
+
+router.get('/:listingId', async (req, res) => {
+    try {
+        const listing = await Listing.findById(req.params.listingId).populate('owner');
+        console.log('Listing: ', listing);
+        res.render('listings/show.ejs', {
+            listing,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
     }
 });
 
