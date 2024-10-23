fff3f437603b7dc0229770994c635124f3b6b668
Yusuf AlHamad <ylive71@gmail.com>
2024-10-23 13:55:24 +0300

    added a post request for new listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -6,12 +6,27 @@ const Listing = require('../models/listing.js');
 router.get('/', async (req, res) => {
     try {
         const listings = await Listing.find({});
-        console.log(listings);
+        console.log('Listing: ', listings);
         res.render('listings/index.ejs');
     } catch (error) {
         console.log(error);
     }
 });
 
+router.get('/new', (req, res) => {
+    res.render('listings/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const listingData = req.body;
+        const owner = req.session.user._id;
+        listingData.owner = owner;
+        await Listing.create(listingData);
+        res.redirect('/listings');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router;
