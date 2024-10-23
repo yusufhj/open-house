571a9911f04a84dc6986b7c7a7c694de6421b4a3
Yusuf AlHamad <ylive71@gmail.com>
2024-10-23 11:25:21 +0300

    added a lisitings landing page and get route

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const Listing = require('../models/listing.js');
 
+router.get('/', async (req, res) => {
+    try {
+        const listings = await Listing.find({});
+        console.log(listings);
+        res.render('listings/index.ejs');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 module.exports = router;
